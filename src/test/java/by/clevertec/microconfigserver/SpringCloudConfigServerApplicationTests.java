297394c82f2bc148f79b73906d9ae391a79da1f1
Viktar Rusakovich <112984431+rusakovich-viktar@users.noreply.github.com>
2024-03-05 20:16:04 +0300

    feat: add files

---
+++ src/test/java/by/clevertec/microconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package by.clevertec.microconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
